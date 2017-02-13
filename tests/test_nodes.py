9d362151312138510bc2276cd86d768384f170a7
André Carvalho <asantostc@gmail.com>
2017-02-13 16:26:12 -0200

    nodes: add list/remove/update operations

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -6,6 +6,9 @@ import httpretty
 
 
 class NodesTestCase(unittest.TestCase):
+
+    cl = client.Client("http://target", "abc123")
+
     def setUp(self):
         httpretty.enable()
 
@@ -23,7 +26,7 @@ class NodesTestCase(unittest.TestCase):
             },
             "Address": "http://127.0.0.3:4243"
         }
-        url = "http://target/docker/node"
+        url = "http://target/1.2/node"
         httpretty.register_uri(
             httpretty.POST,
             url,
@@ -31,17 +34,15 @@ class NodesTestCase(unittest.TestCase):
             status=200
         )
 
-        cl = client.Client("http://target", "abc123")
-
         data = {"address": "127.0.0.3:4243", "pool": "tsuru2", "register": "false"}
-        result = cl.nodes.create(**data)
+        result = self.cl.nodes.create(**data)
 
         self.assertDictEqual(result.json(), node_data)
         self.assertEqual("bearer abc123", httpretty.last_request().headers["authorization"])
         self.assertIn("register", httpretty.last_request().body.decode('utf-8'))
         self.assertIn("false", httpretty.last_request().body.decode('utf-8'))
 
-        result = cl.nodes.create(**data)
+        result = self.cl.nodes.create(**data)
 
         self.assertDictEqual(result.json(), node_data)
         self.assertEqual("bearer abc123", httpretty.last_request().headers["authorization"])
@@ -49,7 +50,7 @@ class NodesTestCase(unittest.TestCase):
         self.assertIn("false", httpretty.last_request().body.decode('utf-8'))
 
         data = {"address": "127.0.0.3:4243", "pool": "tsuru2", "register": "true"}
-        result = cl.nodes.create(**data)
+        result = self.cl.nodes.create(**data)
 
         self.assertDictEqual(result.json(), node_data)
         self.assertEqual("bearer abc123", httpretty.last_request().headers["authorization"])
@@ -57,9 +58,59 @@ class NodesTestCase(unittest.TestCase):
         self.assertIn("true", httpretty.last_request().body.decode('utf-8'))
 
         data = {"address": "127.0.0.3:4243", "pool": "tsuru2", "register": "invalid"}
-        result = cl.nodes.create(**data)
+        result = self.cl.nodes.create(**data)
 
         self.assertDictEqual(result.json(), node_data)
         self.assertEqual("bearer abc123", httpretty.last_request().headers["authorization"])
         self.assertIn("register", httpretty.last_request().body.decode('utf-8'))
         self.assertIn("invalid", httpretty.last_request().body.decode('utf-8'))
+
+    def test_list(self):
+        data = {
+            "nodes": [
+                {
+                    "Status": "ready",
+                    "Metadata": {
+                        "pool": "tsuru2",
+                        "iaas": "ec2",
+                        "LastSuccess": "2015-11-16T18:44:36-02:00",
+                    },
+                    "Address": "http://127.0.0.3:4243"
+                }
+            ]
+        }
+        url = "http://target/1.2/node"
+        httpretty.register_uri(
+            httpretty.GET,
+            url,
+            body=json.dumps(data),
+            status=200
+        )
+
+        result = self.cl.nodes.list()
+        self.assertDictEqual(result, data)
+        self.assertEqual("bearer abc123", httpretty.last_request().headers["authorization"])
+
+    def test_remove(self):
+        url = "http://target/1.2/node/127.0.0.1:4243"
+        httpretty.register_uri(
+            httpretty.DELETE,
+            url,
+            status=200
+        )
+
+        data = {"no-rebalance": "true"}
+        self.cl.nodes.remove(address="127.0.0.1:4243", **data)
+        self.assertEqual("bearer abc123", httpretty.last_request().headers["authorization"])
+
+    def test_update(self):
+        url = "http://target/1.2/node"
+        httpretty.register_uri(
+            httpretty.PUT,
+            url,
+            status=200
+        )
+
+        data = {"enable": "true"}
+        self.cl.nodes.update(address="127.0.0.1:4243", **data)
+        self.assertEqual("bearer abc123", httpretty.last_request().headers["authorization"])
--- tsuruclient/nodes.py
+++ tsuruclient/nodes.py
@@ -13,9 +13,41 @@ class Manager(Base):
         Create a node.
         """
         response = requests.post(
-            "{}/docker/node".format(self.target),
+            "{}/1.2/node".format(self.target),
             data=kwargs,
             headers=self.headers,
             stream=True
+        )
+        return response
+
+    def list(self):
+        """
+        List nodes.
+        """
+        response = requests.get(
+            "{}/1.2/node".format(self.target),
+            headers=self.headers
+        )
+        return response.json()
+
+    def remove(self, address, **kwargs):
+        """
+        Remove a node.
+        """
+        response = requests.delete(
+            "{}/1.2/node/{}".format(self.target, address),
+            headers=self.headers,
+            params=kwargs
+        )
+        return response
+
+    def update(self, address, **kwargs):
+        """
+        Update a node.
+        """
+        response = requests.put(
+            "{}/1.2/node".format(self.target),
+            headers=self.headers,
+            data=kwargs,
         )
         return response
